064c6e9c40b30b0c9ea721d6dd08453892a4fc2c
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 18:13:46 -0300

    register "users" blueprint

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 
 from flask import Flask
 from flask_cors import CORS
-from cookiedb import CookieDB
+
+from blueprints.users import users
 
 import config
 
 app = Flask(__name__)
 cors = CORS(app)
+
+app.register_blueprint(users)
 
 if __name__ == '__main__':
     app.run(
