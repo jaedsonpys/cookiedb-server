ecb9b4d6181f21afca2115aeebf12d3a43250e94
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 17:17:08 -0300

    getting enviroment variables in config file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import dotenv
+
+env = dotenv.dotenv_values('.env')
+
+# enviroment variables
+DATABASE_KEY = env.get('DATABASE_KEY')
+FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
+UTOKEN_SECRET_KEY = env.get('UTOKEN_SECRET_KEY')
