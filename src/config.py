c8eeced19ec888ada05f249a9cfc592511b9517c
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 17:27:02 -0300

    adding host, port and debug mode enviroment variables

--- src/config.py
+++ src/config.py
@@ -7,5 +7,9 @@ SERVER_DATABASE_KEY = env.get('SERVER_DATABASE_KEY')
 FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
 UTOKEN_SECRET_KEY = env.get('UTOKEN_SECRET_KEY')
 
+SERVER_HOST = env.get('SERVER_HOST', '127.0.0.1')
+SERVER_PORT = env.get('SERVER_PORT', 5500)
+SERVER_DEBUG = env.get('SERVER_DEBUG', False)
+
 SERVER_CONFIG_DB = 'cookiedb-server'
 USERS_DB = 'cookiedb-users'
