d7ee8c9e3bd2597d922ff8084409533fce637e00
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 17:19:45 -0300

    adding default database names

--- src/config.py
+++ src/config.py
@@ -6,3 +6,6 @@ env = dotenv.dotenv_values('.env')
 DATABASE_KEY = env.get('DATABASE_KEY')
 FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
 UTOKEN_SECRET_KEY = env.get('UTOKEN_SECRET_KEY')
+
+SERVER_CONFIG_DB = 'cookiedb-server'
+USERS_DB = 'cookiedb-users'
