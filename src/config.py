80b5ba88499b656256fe3e49b88859bb4db40f78
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 17:21:24 -0300

    renaming enviroment variable

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import dotenv
 env = dotenv.dotenv_values('.env')
 
 # enviroment variables
-DATABASE_KEY = env.get('DATABASE_KEY')
+SERVER_DATABASE_KEY = env.get('SERVER_DATABASE_KEY')
 FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
 UTOKEN_SECRET_KEY = env.get('UTOKEN_SECRET_KEY')
 
