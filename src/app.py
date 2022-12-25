bd571a7aaf2f0621f8ea28a803dc570d5c2be202
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 17:31:56 -0300

    config and open users database

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
 
 from flask import Flask
 from flask_cors import CORS
+from cookiedb import CookieDB
 
 import config
 
 app = Flask(__name__)
 cors = CORS(app)
+
+users_db = CookieDB(
+    key=config.SERVER_DATABASE_KEY,
+    database_local='./'
+)
+
+users_db.create_database(config.USERS_DB)
+users_db.open(config.USERS_DB)
 
 if __name__ == '__main__':
     app.run(
