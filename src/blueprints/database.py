aecf1dfcedacb1602245261f472fa622dd9b73b6
Jaedson Silva <contacanal798@gmail.com>
2022-12-25 23:22:59 -0300

    adding function to generate database ID

--- src/blueprints/database.py
+++ src/blueprints/database.py
@@ -1,6 +1,13 @@
+import secrets
+
 import cookiedb
 from flask import Blueprint
 
 from config import users_db
 
 database = Blueprint('database', __name__)
+
+
+def generate_id() -> str:
+    db_id = secrets.token_hex(16)
+    return db_id
