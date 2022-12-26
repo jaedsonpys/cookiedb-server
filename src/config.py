a3ec42ac0b259e7274afda9d1c24d3fb4356cd9e
Jaedson Silva <contacanal798@gmail.com>
2022-12-26 00:31:15 -0300

    get DATABASES_PATH enviroment variable

--- src/config.py
+++ src/config.py
@@ -5,8 +5,9 @@ env = dotenv.dotenv_values('.env')
 
 # enviroment variables
 SERVER_DATABASE_KEY = env.get('SERVER_DATABASE_KEY')
-FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
 UTOKEN_SECRET_KEY = env.get('UTOKEN_SECRET_KEY')
+FLASK_SECRET_KEY = env.get('FLASK_SECRET_KEY')
+DATABASES_PATH = env.get('DATABASES_PATH')
 
 SERVER_HOST = env.get('SERVER_HOST', '127.0.0.1')
 SERVER_PORT = env.get('SERVER_PORT', 5500)
@@ -15,6 +16,7 @@ SERVER_DEBUG = bool(env.get('SERVER_DEBUG', False))
 SERVER_CONFIG_DB = 'cookiedb-server'
 USERS_DB = 'cookiedb-users'
 
+# users database
 users_db = CookieDB(
     key=SERVER_DATABASE_KEY,
     database_local='./'
