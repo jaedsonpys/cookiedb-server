023033b1c78629682e1fb3d946c4f61f93b5c552
Jaedson Silva <contacanal798@gmail.com>
2022-12-27 22:26:49 -0300

    create test_register_user test

--- test/test_api.py
+++ test/test_api.py
@@ -20,6 +20,23 @@ class TestAPI(bupytest.UnitTest):
         if os.path.isfile('./cookiedb-users.cookiedb'):
             os.remove('./cookiedb-users.cookiedb')
 
+    def test_register_user(self):
+        req = requests.post(URL + '/register', json={
+            'username': USER_NAME,
+            'email': USER_EMAIL,
+            'password': USER_PW
+        })
+
+        # expected 201 (Created)
+        self.assert_expected(req.status_code, 201)
+
+        response = req.json()
+
+        self.assert_true(response['token'])
+        self.assert_expected(response['status'], 'success')
+
+        self._token = response['token']
+
 
 if __name__ == '__main__':
     bupytest.this()
