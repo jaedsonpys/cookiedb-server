03bdaf6dff410cb70e18868a299d1f25fb2dde57
Jaedson Silva <contacanal798@gmail.com>
2022-12-27 22:44:57 -0300

    delete generated by server and wait server startup

--- test/test_api.py
+++ test/test_api.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import bupytest
 import requests
 
@@ -10,6 +13,22 @@ USER_PW = '12345678'
 class TestAPI(bupytest.UnitTest):
     def __init__(self):
         super().__init__()
+
+        if os.path.isdir('./database'):
+            shutil.rmtree('./database', ignore_errors=True)
+
+        if os.path.isfile('./cookiedb-users.cookiedb'):
+            os.remove('./cookiedb-users.cookiedb')
+
+        # wait server start
+        print('Start the CookieDB Server in \033[1msrc/app.py\033[m')
+        while True:
+            try:
+                requests.get(URL)
+            except requests.exceptions.ConnectionError:
+                continue
+            else:
+                break
 
     def test_register_user(self):
         req = requests.post(URL + '/register', json={
