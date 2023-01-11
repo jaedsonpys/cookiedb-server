48cf0ec0908a58668d12a24d143a47387ad8ef1c
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 14:28:59 -0300

    create configure() function to save password

--- src/config.py
+++ src/config.py
@@ -1,9 +1,21 @@
 import os
+import hashlib
+import random
 from pathlib import Path
 
 HOME_USER = Path.home()
 COOKIEDB_PATH = os.path.join(HOME_USER, '.cookiedbserver')
+PASSWORD_PATH = os.path.join(COOKIEDB_PATH, 'password')
 
 
-def check_config():
+def check_config() -> None:
     return os.path.isdir(COOKIEDB_PATH)
+
+
+def configure(password: str) -> None:
+    salt = random.randint(0, 10)
+    pw_with_salt = (password + str(salt)).encode()
+    hashed_pw = hashlib.sha256(pw_with_salt)
+
+    with open(PASSWORD_PATH, 'w') as file:
+        file.write(hashed_pw)
