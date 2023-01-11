04a4b53ee32f06eb00beed48fd91c861d947c1aa
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 15:22:55 -0300

    group functions in DBHandle class

--- src/database.py
+++ src/database.py
@@ -6,14 +6,16 @@ from .config import PASSWORD_PATH
 with open(PASSWORD_PATH, 'r') as file:
     DATABASE_KEY = file.read()
 
-global_db = cookiedb.CookieDB(DATABASE_KEY, DATABASES_PATH)
 
+class DBHandle:
+    def __init__(self) -> None:
+        self._db = cookiedb.CookieDB(DATABASE_KEY, DATABASES_PATH)
 
-def list_databases() -> list:
-    db_files = os.listdir(DATABASES_PATH)
-    db_files = [f.replace('.cookiedb', '') for f in db_files]
-    return db_files
+    @staticmethod
+    def list_databases() -> list:
+        db_files = os.listdir(DATABASES_PATH)
+        db_files = [f.replace('.cookiedb', '') for f in db_files]
+        return db_files
 
-
-def create_database(name: str) -> None:
-    global_db.create_database(name, if_not_exists=name.endswith('?'))
+    def create_database(self, name: str) -> None:
+        self._db.create_database(name, if_not_exists=name.endswith('?'))
