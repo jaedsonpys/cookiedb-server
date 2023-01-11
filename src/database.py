2b01e211c2f609b4a5451d5a38b2a05c5a420848
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 15:15:48 -0300

    add function to create user database

--- src/database.py
+++ src/database.py
@@ -11,3 +11,8 @@ def list_databases() -> list:
     db_files = os.listdir(DATABASES_PATH)
     db_files = [f.replace('.cookiedb', '') for f in db_files]
     return db_files
+
+
+def create_database(name: str) -> None:
+    db = cookiedb.CookieDB(DATABASE_KEY, DATABASES_PATH)
+    db.create_database(name, if_not_exists=name.endswith('?'))
