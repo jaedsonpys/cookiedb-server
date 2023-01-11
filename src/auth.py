7cb4b1fdd06b373e3bc1cc2c3cc228c30a9bb287
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 14:38:20 -0300

    create Auth._check_password() method

--- src/auth.py
+++ src/auth.py
@@ -1,63 +1,20 @@
-# CookieDB database server
-# Copyright (C) 2023  Jaedson Silva
+import hashlib
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-from functools import wraps
-
-import utoken
-
-from flask import request
-from flask import jsonify
-
-from config import UTOKEN_SECRET_KEY
-from config import users_db
+from .config import PASSWORD_PATH
 
 
-def _users_exists(email: str) -> bool:
-    user = users_db.get(f'users/{email}')
-    return bool(user)
+class Auth:
+    @staticmethod
+    def _check_password(password: str) -> bool:
+        with open(PASSWORD_PATH, 'rb') as file:
+            hashed_pw = file.read()
 
+        for salt in range(1, 10):
+            pw_and_salt = (password + str(salt)).encode()
+            try_pw_hash = hashlib.sha256(pw_and_salt)
 
-def required_auth(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        authorization = request.headers.get('Authorization')
+            if hashed_pw == try_pw_hash:
+                # password match
+                return True
 
-        if authorization:
-            auth_info = authorization.split(' ')
-
-            if len(auth_info) == 2 and auth_info[0] == 'Bearer':
-                token = auth_info[1]
-                utoken_exceptions = (
-                    utoken.exceptions.ExpiredTokenError,
-                    utoken.exceptions.InvalidKeyError,
-                    utoken.exceptions.InvalidTokenError,
-                    utoken.exceptions.InvalidContentTokenError
-                )
-
-                try:
-                    payload = utoken.decode(token, UTOKEN_SECRET_KEY)
-                except utoken_exceptions:
-                    response = jsonify(status='error', message='new_token_required'), 401
-                else:
-                    response = func(payload, *args, **kwargs)
-            else:
-                response = jsonify(status='error', message='bearer_auth_required'), 401
-        else:
-            response = jsonify(status='error', message='bearer_auth_required'), 401
-
-        return response
-
-    return wrapper
+        return False
