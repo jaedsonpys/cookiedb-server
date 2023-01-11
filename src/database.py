8a11570b4e684544fbed0a1d6e22d8c5c8f1a5f5
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 15:28:10 -0300

    add _delete_database to DBHandle class

--- src/database.py
+++ src/database.py
@@ -17,5 +17,12 @@ class DBHandle:
         db_files = [f.replace('.cookiedb', '') for f in db_files]
         return db_files
 
+    @staticmethod
+    def _delete_database(self, name: str) -> None:
+        db_file = os.path.join(DATABASES_PATH, f'{name}.cookiedb')
+
+        if os.path.isfile(db_file):
+            os.remove(db_file)
+
     def _create_database(self, name: str) -> None:
         self._db.create_database(name, if_not_exists=name.endswith('?'))
