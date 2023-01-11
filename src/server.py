f978c084d07415ab468366b3a3ef41803adb133f
Jaedson Silva <contacanal798@gmail.com>
2023-01-11 13:58:38 -0300

    add Server.run() method to run server in background

--- src/server.py
+++ src/server.py
@@ -1,14 +1,23 @@
 import socket
+import threading
 
 
 class Server:
     def __init__(self, host: str = '127.0.0.1') -> None:
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.bind((host, 2808))
+
+        self._address = (host, 2808)
+        self._socket.bind(self._address)
         self._listen(5)
 
     def _run(self) -> None:
         while True:
             client, addr = self._socket.accept()
             data = client.recv(1024)
+
+    def run(self) -> None:
+        print(f'Server started at {self._address[0]}:{self._address[1]}')
+
+        server_th = threading.Thread(target=self._run)
+        server_th.start()
