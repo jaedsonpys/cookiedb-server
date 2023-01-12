7ba83064432ebff9ac1b6998f9d03c133b352dd1
Jaedson Silva <contacanal798@gmail.com>
2023-01-12 19:34:00 -0300

    config server if server files not available

--- src/__main__.py
+++ src/__main__.py
@@ -30,3 +30,24 @@ def main():
                 print('\nServer stopped.')
                 sock.stop()
                 return 0
+    else:
+        print('\033[32mWelcome to CookieDB Server!\033[m')
+        print('Set a password to access your database.')
+        print('Connections will only be allowed if the password is correct.\n')
+        
+        try:
+            password = getpass.getpass('[?] Password: ')
+            password = password.strip()
+
+            confirm_pw = getpass.getpass('[?] Confirm the password: ')
+            confirm_pw = confirm_pw.strip()
+
+            while password != confirm_pw:
+                confirm_pw = getpass.getpass('[?] Confirm the password (\033[31mTry Again\033[m): ')
+                confirm_pw = confirm_pw.strip()
+        except KeyboardInterrupt:
+            print()
+            return 0
+
+        config.configure(password)
+        print('\n\u2705 All right! Now you can run the server smoothly.')
