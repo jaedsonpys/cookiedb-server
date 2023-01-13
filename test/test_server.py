c55c924f822d4c98424b69d519d86361c408cc8f
Jaedson Silva <contacanal798@gmail.com>
2023-01-13 19:57:18 -0300

    create Server class instance in unit test

--- test/test_server.py
+++ test/test_server.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import shutil
 
 import bupytest
+
+sys.path.insert(0, './')
+
+from src.server import Server
 
 HOST = '127.0.0.1'
 USER_PW = '12345678'
@@ -24,7 +29,10 @@ class TestServer(bupytest.UnitTest):
                 'inStock': False
             }
         }
-        
+
+        server = Server()
+        server.run()
+
 
 if __name__ == '__main__':
     bupytest.this()
