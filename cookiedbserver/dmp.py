c44988e3152868b18faf6f864fce792cd90b9412
Jaedson Silva <contacanal798@gmail.com>
2023-01-14 16:39:06 -0300

    parse received data from client

--- cookiedbserver/dmp.py
+++ cookiedbserver/dmp.py
@@ -16,6 +16,7 @@
 # STATUS MESSAGE
 # <data (required if requested "action" is GET, ADD, DEL, or UPD)>
 
+import json
 import struct
 
 
@@ -28,12 +29,18 @@ class DMP:
         action = struct.unpack(f'3s', action_field)
         action = action[0].decode()
 
-        remaining_fields = message[3:]
+        splited_msg = message.split(b'\n')
+        remaining_fields = splited_msg[0][3:]
         fields = struct.unpack(f'{len(remaining_fields)}s', remaining_fields)
 
         if action in ('GET', 'ADD', 'UPD', 'DEL'):
             path, database = fields[0].split(b':')
             request['database'] = database.decode()
+
+            if action in ('ADD', 'UPD'):
+                data = splited_msg[1]
+                json_data = json.loads(data)
+                request['data'] = json_data
         elif action in ('CDB', 'LDB', 'DDB', 'ODB'):
             path = fields[0]
 
@@ -48,4 +55,5 @@ if __name__ == '__main__':
     print(DMP.parse_request(request_data))
 
     request_data = struct.pack('3s 11s', b'ADD', b'users/:mydb')
+    request_data += b'\n' + (json.dumps({'name': 'Jaedson'})).encode()
     print(DMP.parse_request(request_data))
